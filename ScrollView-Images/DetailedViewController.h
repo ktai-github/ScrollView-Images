b7d7ac4ae2719ea384a186cc6795efd4fe0614a6
Kevin T <kevin.tai.mail@gmail.com>
2018-02-26 19:54:41 -0800

    part 2 done

---
+++ ScrollView-Images/DetailedViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailedViewController.h
+//  ScrollView-Images
+//
+//  Created by KevinT on 2018-02-26.
+//  Copyright © 2018 KevinT. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface DetailedViewController : ViewController
+
+@end
---
+++ ScrollView-Images/DetailedViewController.m
@@ -0,0 +1,43 @@
+//
+//  DetailedViewController.m
+//  ScrollView-Images
+//
+//  Created by KevinT on 2018-02-26.
+//  Copyright © 2018 KevinT. All rights reserved.
+//
+
+#import "DetailedViewController.h"
+
+@interface DetailedViewController () <UIScrollViewDelegate>
+@property (weak, nonatomic) IBOutlet UIScrollView *detailedScrollView;
+@property (weak, nonatomic) IBOutlet UIImageView *detailedImageView;
+
+@end
+
+@implementation DetailedViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (nullable UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView {
+  return self.detailedImageView;
+}     // return a view that will be scaled. if delegate returns nil, nothing happens
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
