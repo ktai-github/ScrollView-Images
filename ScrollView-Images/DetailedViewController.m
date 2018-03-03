d44bc6c540aa46623b03b4d44964787a23905d14
Kevin T <kevin.tai.mail@gmail.com>
2018-03-03 13:14:36 -0800

    pinch implemented on detailed view

--- ScrollView-Images/DetailedViewController.m
+++ ScrollView-Images/DetailedViewController.m
@@ -21,6 +21,26 @@
     // Do any additional setup after loading the view.
   self.detailedImageView.image = self.image;
   
+//  CGFloat width = 100;
+//  CGFloat height = 100;
+//
+//  CGRect frame = CGRectMake(CGRectGetMidX(self.view.bounds) - width/2, CGRectGetMidY(self.view.bounds) - height/2, width, height);
+//
+//  UIView *view = [[UIView alloc] initWithFrame:frame];
+  
+//  view.backgroundColor = [UIColor purpleColor];
+//  [self.view addSubview:view];
+  
+  UIPinchGestureRecognizer *pinchGesture = [[UIPinchGestureRecognizer alloc] initWithTarget:self action:@selector(viewPinched:)];
+  [self.detailedScrollView addGestureRecognizer:pinchGesture];
+
+}
+
+- (void)viewPinched:(UIPinchGestureRecognizer *) sender {
+  CGFloat scale = sender.scale;
+  sender.view.transform = CGAffineTransformMakeScale(scale, scale);
+  
+  scale = 1.0;
 }
 
 - (void)didReceiveMemoryWarning {
